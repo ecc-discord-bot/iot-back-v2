f2cc300ada86224438b2f1cea470fe1c75a77d9d
mattuu0 <tempacountyt@gmail.com>
2025-04-13 16:44:56 +0900

    mysql の初回起動時にデータベースと ユーザーを作成するスクリプトを作成

---
+++ app/src/models/user.go
@@ -0,0 +1,12 @@
+package models
+
+type User struct {
+	Discord_id  string `gorm:"primary_key"`
+	Students_id string
+	Name        string
+	Class       string
+	Signature   string
+	NowTime     int64
+	Is_paid     bool
+	Is_agreed   bool
+}
