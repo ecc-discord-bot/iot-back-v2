213e8fd084f66df7cbe23c31c5f3f5d7258d0f26
mattuu0 <tempacountyt@gmail.com>
2025-04-13 15:50:31 +0900

    環境追加

---
+++ auth/src/models/dbconn.go
@@ -0,0 +1,47 @@
+package models
+
+import (
+	"gorm.io/driver/mysql"
+ 	"gorm.io/gorm"
+)
+
+var (
+	dbconn *gorm.DB = nil
+)
+
+func OpenDB() (*gorm.DB,error) {
+	dsn := "main:main@tcp(db:3306)/maindb?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// エラー処理
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func Init() error {
+	// データベース接続
+	db, err := OpenDB()
+	if err != nil {
+		return err
+	}
+
+	// データベース接続確認
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Provider{})
+	db.AutoMigrate(&Session{})
+
+	// グローバル変数に格納
+	dbconn = db
+
+	// プロバイダを初期化する
+	InitProviders()
+
+	return nil
+}
+
+func GetDB() *gorm.DB {
+	return dbconn
+}
